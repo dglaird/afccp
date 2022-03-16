aa3c86a7f9b64e13ba71f829502fed6f7a33a8ab
Daniel Griffen Laird <griffenlaird007@gmail.com>
2022-03-16 00:18:53 -0400

    changes

--- afccp/executables/laird/test_pc.py
+++ afccp/executables/laird/test_pc.py
@@ -5,4 +5,8 @@ dir_path = os.getcwd() + '/'
 
 # Import main problem class
 from afccp.core.problem_class import CadetCareerProblem
-pass
+
+instance = CadetCareerProblem('Realistic', printing=True)
+instance.import_default_value_parameters(no_constraints=True)
+instance.solve_vft_pyomo_model(max_time=10)
+instance.export_to_excel()
