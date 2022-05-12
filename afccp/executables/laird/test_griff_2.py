207676e765d927d6b6fb2fde90ebdb112d7dfe86
Daniel Griffen Laird <68079444+dglaird@users.noreply.github.com>
2022-05-12 18:57:18 -0500

    small tests

--- afccp/executables/laird/test_griff_2.py
+++ afccp/executables/laird/test_griff_2.py
@@ -14,6 +14,7 @@ from afccp.core.problem_class import CadetCareerProblem
 
 instance = CadetCareerProblem('2021', printing=True)
 instance.set_instance_value_parameters()
+instance.full_vft_model_solve(ga_max_time=60, ga_printing=True)
 # instance.vft_to_gp_parameters(get_new_rewards_penalties=True, provide_executable=True)
 # instance.export_to_excel()
 
